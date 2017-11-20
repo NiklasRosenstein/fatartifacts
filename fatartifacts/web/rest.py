f0a045ff70e59bf45a11ff064151dd7c85557fc4
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-20 11:00:40 +0100

    update REST URLs and add REST API for Database.get_artifact_object() as well as uploading a new artifact with a PUT request

--- fatartifacts/web/rest.py
+++ fatartifacts/web/rest.py
@@ -1,7 +1,7 @@
 
 from .fablueprint import FaBlueprint
 from ..base.database import ArtifactDoesNotExist
-from flask import abort, redirect, url_for, send_file
+from flask import abort, redirect, request, url_for, send_file
 from flask_restful import Api, Resource
 
 app = FaBlueprint(__name__, __name__)
@@ -9,6 +9,12 @@ api = Api(app)
 
 # XXX Use app.accesscontrol to determine accessibility.
 # XXX Implement creation/deletion of artifacts (with respect to app.accesscontrol).
+
+def get_object_url(group_id, artifact_id, version, obj):
+  if obj.has_web_uri():
+    return obj.uri
+  return url_for(__name__ + '.read',
+    group_id=group_id, artifact_id=artifact_id, version=version, tag=obj.tag)
 
 
 class ListGroupIds(Resource):
@@ -34,20 +40,42 @@ class ListObjects(Resource):
   def get(self, group_id, artifact_id, version):
     result = {}
     for o in app.database.get_artifact_objects(group_id, artifact_id, version):
-      if o.has_web_uri():
-        url = o.uri
-      else:
-        url = url_for(__name__ + '.read',
-          group_id=group_id, artifact_id=artifact_id, version=version,
-          tag=o.tag)
+      url = get_object_url(group_id, artifact_id, version, o)
       result[o.tag] = {'filename': o.filename, 'url': url}
     return result
 
 
-api.add_resource(ListGroupIds, '/list/')
-api.add_resource(ListArtifactIds, '/list/<group_id>')
-api.add_resource(ListVersions, '/list/<group_id>/<artifact_id>')
-api.add_resource(ListObjects, '/list/<group_id>/<artifact_id>/<version>')
+class Object(Resource):
+
+  def get(self, group_id, artifact_id, version, tag):
+    try:
+      o = app.database.get_artifact_object(group_id, artifact_id, version, tag)
+    except ArtifactDoesNotExist:
+      abort(404)
+    url = get_object_url(group_id, artifact_id, version, o)
+    return {'tag': o.tag, 'filename': o.filename, 'url': url}
+
+  def put(self, group_id, artifact_id, version, tag):
+    src = request.files.get('file')
+    if not src:
+      abort(400)
+    mime = request.data.get('mimetype')
+    if not mime:
+      abort(400)
+    filename = secure_filename(src.name)
+    dst, uri = app.storage.open_write_file(group_id, artifact_id, version, tag, filename)
+    with dst:
+      shutil.copyfileobj(src, dst)
+      obj = ArtifactObject(tag, filename, uri, mime)
+      app.database.create_artifact(group_id, artifact_id, version, obj)
+    return {'message': "Artifact created."}
+
+
+api.add_resource(ListGroupIds, '/')
+api.add_resource(ListArtifactIds, '/<group_id>')
+api.add_resource(ListVersions, '/<group_id>/<artifact_id>')
+api.add_resource(ListObjects, '/<group_id>/<artifact_id>/<version>')
+api.add_resource(Object, '/<group_id>/<artifact_id>/<version>/<tag>')
 
 
 @app.route('/read/<group_id>/<artifact_id>/<version>/<tag>')
