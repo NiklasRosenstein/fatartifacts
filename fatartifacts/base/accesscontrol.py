f33f30042bd64c65dfe73049404f6d42a87f4970
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-20 11:56:51 +0100

    add useful classmethods to Permissions, add isolate argument to SimpleAccesControl constructor

--- fatartifacts/base/accesscontrol.py
+++ fatartifacts/base/accesscontrol.py
@@ -11,6 +11,18 @@ class Permissions(NamedTuple):
   can_write: bool
   can_delete: bool
 
+  @classmethod
+  def allow_all(cls):
+    return cls(True, True, True)
+
+  @classmethod
+  def deny_all(cls):
+    return cls(False, False, False)
+
+  @classmethod
+  def read_only(cls):
+    return cls(True, False, False)
+
 
 class AccessControl(metaclass=abc.ABCMeta):
 
--- fatartifacts/contrib/simpleac.py
+++ fatartifacts/contrib/simpleac.py
@@ -11,13 +11,17 @@ from typing import *
 
 class SimpleAccessControl(accesscontrol.AccessControl):
 
-  def __init__(self, limit_to_prefix=True):
-    self.limit_to_prefix = limit_to_prefix
+  def __init__(self, enforce_prefix=True, isolate=False):
+    self.enforce_prefix = enforce_prefix
+    self.isolate = isolate
 
   def get_group_permissions(self, account: Optional[str], group_id: str) -> accesscontrol.Permissions:
-    if not account or (self.limit_to_prefix and not group_id.startswith(account + '.')):
-      return accesscontrol.Permissions(can_read=True, can_write=False, can_delete=False)
-    return accesscontrol.Permissions(can_read=True, can_write=True, can_delete=True)
+    if not account or (self.enforce_prefix and not group_id.startswith(account + '.')):
+      if self.isolate:
+        return accesscontrol.Permissions.deny_all()
+      else:
+        return accesscontrol.Permissions.read_only()
+    return accesscontrol.Permissions.allow_all()
 
   def get_artifact_permissions(self, account: Optional[str], group_id: str, artifact_id: str) -> accesscontrol.Permissions:
     return self.get_group_permissions(account, group_id)
